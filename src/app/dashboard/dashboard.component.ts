1d3b2011eda6134cbd34b8f313bde14038943ae9
gunni <gunni1111@gmail.com>
2020-05-03 10:52:47 +0200

    dashboard component for menu

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigateByUrl("/home")
+  }
+
+  ngOnInit() {
+  }
+
+}
